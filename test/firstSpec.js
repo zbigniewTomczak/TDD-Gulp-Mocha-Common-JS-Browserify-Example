a1f681e593db4a827f07c9de8e64203f44b4725e
Zbigniew Tomczak <zbigniew.tomczak@pl.abb.com>
2016-11-11 08:43:10 +0100

    Correct assertion arguments order and html usage example

--- test/firstSpec.js
+++ test/firstSpec.js
@@ -3,7 +3,7 @@ var first = require('../src/first');
 
 describe('myFirst', function() {
     it('returns message from second', function() {
-        expect('second').to.equal(first.myFirst());
+        expect(first.myFirst()).to.equal('second');
     });
 });
 
