d5e6a5775ea55105edb8696af9d4cb05ece7de42
Zbigniew Tomczak <zbigniew.tomczak@pl.abb.com>
2016-11-11 09:13:23 +0100

    watchify watches for file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 
+var brify = browserify({
+    entries: ['./src/app'],
+    cache: {},
+    packageCache: {},
+    plugin: [watchify]
+});
+
+function bundle() {
+    return brify.bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('build'));
+}
 
 gulp.task('test', function() {
     return gulp.src('test/*.js')
@@ -17,5 +30,10 @@ gulp.task('default', function() {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+    brify.on('update', bundle);
+    return bundle();
 });
 
